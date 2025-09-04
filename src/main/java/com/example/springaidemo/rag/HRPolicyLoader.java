b24ff2cf4ce191b39e416e18f1feac62bcc23544
Tahmid Akter <tahmidakter56@gmail.com>
2025-09-05 02:20:16 +0600

    text splitting added for documents

--- src/main/java/com/example/springaidemo/rag/HRPolicyLoader.java
+++ src/main/java/com/example/springaidemo/rag/HRPolicyLoader.java
@@ -3,6 +3,8 @@ package com.example.springaidemo.rag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -30,6 +32,9 @@ public class HRPolicyLoader {
 
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(policyFile);
         List<Document> docs = tikaDocumentReader.get();
-        vectorStore.add(docs);
+
+        TextSplitter textSplitter = TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(400).build();
+
+        vectorStore.add(textSplitter.split(docs));
     }
 }
